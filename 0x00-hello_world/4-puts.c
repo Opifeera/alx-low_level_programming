93cef24ef4c3fb86282ff9160abd55722faacad6
Opifeera <opifeerafernando@gmail.com>
2022-10-20 05:50:45 -0700

    edit 4 for puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-/*
-*Main function
+
+/**
+* Main function entry point
+* 
+* Prints with puts function
+*
+* Always return 0
 */
+
 int main(void)
 {
 	char strg[] = "\"Programming is like building a multilingual puzzle";
