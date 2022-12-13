c23bd64959554f6d03d7415ca835cf9ba9b81f23
Opifeera <opifeerafernando@gmail.com>
2022-12-13 12:56:02 -0800

    Prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	while (h < 24)
+	{
+		while (min < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar ((min % 10) + '0');
+			_putchar('\n');
+			min++
+		}
+
+		hour++;
+	}
+}
