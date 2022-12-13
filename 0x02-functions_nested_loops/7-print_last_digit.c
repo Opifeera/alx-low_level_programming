3105a93411e75cecc5ca83db265545a24fb61636
Opifeera <opifeerafernando@gmail.com>
2022-12-13 12:35:24 -0800

    prints last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @last: digit
+ *
+ * Return: Last digit of a number
+ */
+int print_last_digit(int last)
+{
+	return (last % 10);
+}
