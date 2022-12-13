77189a68cabf226e1a562221cf8290f9ac82546c
Opifeera <opifeerafernando@gmail.com>
2022-12-13 10:59:04 -0800

    Printing lower case characters with _putchar user defined function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lower, i;
+	i = a;
+
+	for(i = 'a', i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
