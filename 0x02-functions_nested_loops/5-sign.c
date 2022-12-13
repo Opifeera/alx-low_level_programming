0e3680ac6dcb0d48548dfa25e9fa70c080c70069
Opifeera <opifeerafernando@gmail.com>
2022-12-13 12:13:45 -0800

    Checks for the sign of a given number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a sign and after checking it
+ *
+ * @n: number to be checked
+ *
+ * Return: After checking
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+}
