1d6f76f8bf77c2b7cbdd0cef0a1c4091ac8b8f6e
Opifeera <opifeerafernando@gmail.com>
2022-12-13 13:31:26 -0800

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,11 +17,11 @@ void times_table(void)
 		for (col = 1; col <= 9; col++)
 		{
 			n = (row * col);
-			if ((d / 10) > 0)
+			if ((n / 10) > 0)
 				_putchar((n / 10) + '0');
 			else
 				_putchar(' ');
-			_putchar((d % 10) + '0');
+			_putchar((n % 10) + '0');
 
 			if (col < 9)
 			{
