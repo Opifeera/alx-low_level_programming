0c4ce9edbe1f99d2c5891c4ff073d9fdb5497f28
Opifeera <opifeerafernando@gmail.com>
2022-12-16 06:58:35 -0800

    Printing numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers 0 to 9
+ * print_numbers - Printing numbers 0 to 9
  *
  * Return: Always 0
  */
@@ -10,6 +10,6 @@ void print_numbers(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
-		_putchar(n + '0');
+		_putchar(n);
 	_putchar('\n');
 }
