3b0e98da26951508a49995b6d86bcf579f3e5333
Opifeera <opifeerafernando@gmail.com>
2022-12-16 03:01:35 -0800

    Function tests presence of upper case charater

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - determines if character is upper case
+ *
+ * @c: character to be tested
+ */
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
