1577af1cd787a66e73432ee5951f76fd2f04a969
Opifeera <opifeerafernando@gmail.com>
2022-12-16 07:31:43 -0800

    prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int row, col;
+
+	while (row < 10)
+	{
+		while (col <= 14)
+		{
+			if (col >= 10)
+				_putchar((col / 10) + '0');
+			_putchar((col % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
