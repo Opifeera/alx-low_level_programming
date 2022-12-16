8b7bb0ced75342f18ca59e5727e5fc188ad42dc7
Opifeera <opifeerafernando@gmail.com>
2022-12-16 11:07:48 -0800

    The fizz buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,14 @@ int main(void)
 	{
 		if (num == 100)
 			printf("Buzz ");
-		else if ((num % 3) == 0)
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			print("FizzBuzz ");
+		else if (num % 3 == 0)
 			printf("Fizz ");
-		else if ((num % 5) == 0)
+		else if (num % 5 == 0)
 			printf("Buzz ");
-		else if (((num % 3) == 0) && ((num % 5) == 0))
-			printf("FizzBuzz ");
 		else
-		{
 			printf("%d ", num);
-		}
 	}
 	printf("\n");
 	return (0);
