bdbf4497092c33175fa4b1303788b31c9cd53eb8
Opifeera <opifeerafernando@gmail.com>
2022-12-16 09:48:43 -0800

    Drawing a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,13 @@ void print_diagonal(int n)
 {
 	int v, h;
 
-	if(n <= 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (v = 1; v <= n; v++)
 		{
-			for (h = 1; h <= v; h++)
+			for (h = 1; h < v; h++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
