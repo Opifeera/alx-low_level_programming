26a35ac1e208f8a017b16d30239b99891ea01eed
Opifeera <opifeerafernando@gmail.com>
2022-12-19 13:07:18 -0800

    Reverse printing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _putchar(char);
+
+/**
+ * print_rev - Prints in reverse
+ *
+ * @s: input
+ *
+ * Return: Reverse order
+ */
+void print_rev(char *s)
+{
+	int foward = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+		forward++;
+	for (n = (forward - 1); n >= 0; n--)
+		_putchar(s[n]);
+	_putchar('\n');	
+}
