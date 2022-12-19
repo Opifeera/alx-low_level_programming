25fd8b25253ad32b69828759de93cacc391ca083
Opifeera <opifeerafernando@gmail.com>
2022-12-19 13:52:08 -0800

    prints half the array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ *
+ * @a: input 1
+ * @n: input 2
+ *
+ * Return: an array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
+		printf("\n");
+}
