454576d3f730db1981dd92c0d2d00657a0cdae75
Opifeera <opifeerafernando@gmail.com>
2022-12-19 14:05:59 -0800

    String copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copy one string to another
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j = 0;
+
+	for (: src[j] != '\0'; j++)
+		dest[j] = src[j];
+	return (dest);
+}
