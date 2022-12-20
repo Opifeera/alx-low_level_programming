7b9f67134860ab91d88d9fadd8964505cdf36ab1
Opifeera <opifeerafernando@gmail.com>
2022-12-20 13:07:28 -0800

    Copy from src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,8 @@
 /**
  * char *_strcpy - copy one string to another
  *
- * @dest: input
- * @src: input
+ * @dest: copy to
+ * @src: copy from
  *
  * Return: string
  */
@@ -12,7 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0'; j++)
+	while (src[j] != '\0')
+	{
 		dest[j] = src[j];
+		j++
+	}
 	return (dest);
 }
