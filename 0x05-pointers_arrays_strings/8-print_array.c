121b80fc2bd4d16bd2545840bea6fc1792bff50c
Opifeera <opifeerafernando@gmail.com>
2022-12-20 12:30:07 -0800

    Printing elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", *(a + i));
+		if (i == (n - 1))
+			break;
+		else
+			printf("%d, ", *(a + i));
 	}
 	printf("\n");
 }
