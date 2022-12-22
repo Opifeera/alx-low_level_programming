094c548dce5b989e07cee9c2146ba903fd714bfc
Opifeera <opifeerafernando@gmail.com>
2022-12-22 13:08:37 -0800

    fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 {
 	int temp, i;
 
-	for (i = (n - 1); i >= (n / 2); i--)
+	for (i = (n - 1); i >= (n / 1); i--)
 	{
 		temp = a[n - 1 - i];
 		a[n - 1 - i] = a[i];
