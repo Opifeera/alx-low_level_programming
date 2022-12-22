e4205e1946b3d57575b162c19f2acdd9ee7659c5
Opifeera <opifeerafernando@gmail.com>
2022-12-22 13:55:15 -0800

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,26 +14,25 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
-			if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				i == 0)
+			i++;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			i == 0)
 			{
 				str[i] -= 32;
 			}
-			i++
-		}
+		i++
 	}
 	return (str);
 }
