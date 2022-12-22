bc3aa9a2772ead244966dec45d4f529aa2d8605e
Opifeera <opifeerafernando@gmail.com>
2022-12-22 01:43:23 -0800

    Concatenate two strings with src of n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,9 +15,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i++])
 		len++;
-	
-	for (i = 0; src[i] && i <= n; i++)
-		dest[len++] = src[i];
 
+	for (i = 0; src[i] && i < n; i++)
+		dest[len++] = src[i];
 	return (dest);
 }
