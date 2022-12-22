18f2b25c87cb97f9e43b6d864726954a0a979460
Opifeera <opifeerafernando@gmail.com>
2022-12-22 01:40:03 -0800

    Concatenate two strings with src of n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two string using n bytes of src
+ *
+ * @dest: first string param
+ * @src: second string param
+ * @n: number of bytes
+ *
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, len = 0;
+
+	while (dest[i++])
+		len++;
+	
+	for (i = 0; src[i] && i <= n; i++)
+		dest[len++] = src[i];
+
+	return (dest);
+}
