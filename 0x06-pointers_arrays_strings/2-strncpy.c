9a412d18fc3bb2c9edaf5087b832af87d915afff
Opifeera <opifeerafernando@gmail.com>
2022-12-22 02:05:25 -0800

    Copying strings from one to another

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strncpy - copies src to dest
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, len = 0;
+
+	while (src[i++])
+		len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = len; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
