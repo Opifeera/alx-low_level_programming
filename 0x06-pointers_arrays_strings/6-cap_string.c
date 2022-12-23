05c0b80d3cb32fa4d2b7fe371fd1f03daa34cb59
Opifeera <opifeerafernando@gmail.com>
2022-12-23 01:58:28 -0800

    The leet challenge

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,6 +15,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
+
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leetch - the leet challenge
+ *
+ * @y: characters param
+ *
+ * Return: String
+ */
+char *leetch(char *y)
+{
+	int i = 0, j, k = 5;
+
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char tw[5] = {'4', '3', '0', '7', '1'};
+
+	while (y[i])
+	{
+		j = 0;
+
+		while (j < k)
+		{
+			if(y[i] == tr[j] || y[i] - 32 == tr[j])
+				y[i] = tw[j];
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
