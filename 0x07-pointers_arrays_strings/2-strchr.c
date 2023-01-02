79ec3c9134981df5584ae135c31e049cdd6a73d3
Opifeera <opifeerafernando@gmail.com>
2023-01-02 13:12:29 -0800

    Locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string in which a character is to be found
+ * @c: character to be found
+ *
+ * Return: located character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '/0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
