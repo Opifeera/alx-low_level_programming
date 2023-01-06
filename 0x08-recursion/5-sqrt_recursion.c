d0c8248420a83ccff68fcc2627cbe54ebe792279
Opifeera <opifeerafernando@gmail.com>
2023-01-05 19:00:39 -0800

    Returns square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * sqrt - finds natural square root of number
+ *
+ * @i: value 1
+ * @rt: value 2
+ *
+ * Return: as conditioned
+ */
+int sqrt(int i, int rt)
+{
+	if (rt * rt == i)
+		return rt;
+	if (rt == (i / 2))
+		return (-1);
+	return (sqrt(i, (rt + 1)));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	int rt = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (sqrt(n, rt));
+}
+
