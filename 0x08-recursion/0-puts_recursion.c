2c74aa77adcbcdadabc048d87c510a361c7bd525
Opifeera <opifeerafernando@gmail.com>
2023-01-05 18:06:39 -0800

    function prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _putchar(char c);
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Functions that call themselves
