0b31df9dee7969548efce6ae39991bf5b93188a9
Opifeera <opifeerafernando@gmail.com>
2023-01-09 14:28:20 -0800

    Program prints product of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program multplies two numbers
+ *
+ * @argc: counts arguments
+ * @argv: strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", (i*j));
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
