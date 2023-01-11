2dd816a38ebb2cb90de75ad6617195a9d73843d3
Opifeera <opifeerafernando@gmail.com>
2023-01-11 13:42:01 -0800

    returns pointer to grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	box = malloc (sizeof(int *) * height);
+	box = malloc(sizeof(int *) * height);
 
 	if (box == NULL)
 		return (NULL);
