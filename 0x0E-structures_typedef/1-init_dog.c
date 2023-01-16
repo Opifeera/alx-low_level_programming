1429d4fcc25af73a2f2661fb1d8891f833829ddf
Opifeera <opifeerafernando@gmail.com>
2023-01-16 10:46:12 -0800

    initializing a structure using a function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type dog
+ *
+ * @d: param 1
+ * @name: param 2
+ * @age: age
+ * @owner: owner
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
