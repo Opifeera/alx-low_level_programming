3910355a8cee2389f85f04fae80275d1f04c7748
Opifeera <opifeerafernando@gmail.com>
2023-01-16 09:56:38 -0800

    My first struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Dealing with strutures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ *
+ * Description: helps create a kind of a class
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
