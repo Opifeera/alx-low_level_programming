927d029d00a7763753cfb6e9920536ad800f47a9
Opifeera <opifeerafernando@gmail.com>
2023-01-19 09:20:37 -0800

    A function that performs an action on an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
  *
  * @array: the array to be taken
  * @size: size of the array
