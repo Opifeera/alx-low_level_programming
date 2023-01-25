41ffcd26c5005dcdd349182a69af6556986d9a2f
Opifeera <opifeerafernando@gmail.com>
2023-01-25 11:10:59 -0800

    Sums all arguments passed

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdargs.h>
+
+/**
+ * sum_them_all - sums all aguments
+ *
+ * @n: number of args
+ *
+ * Return: sum of all args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
